d4fefb3e3969252657911342b2712db37717b59b
mamatagupta2026 <mgupta@archaea-ai.com>
2026-03-12 15:28:33 +0530

    Update test3.py

--- test3.py
+++ test3.py
@@ -5,6 +5,7 @@ from app.core.config import settings
 
 def main():
     uvicorn.run("main:app", host=settings.APP_HOST, port=settings.APP_PORT, reload=settings.DEBUG, workers=5)
+    print("hello")
 
 
 if __name__ == "__main__":
