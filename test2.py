b86a2248241a06c3d6c67203bfb40436ba6c30d2
mamatagupta2026 <mgupta@archaea-ai.com>
2026-03-12 14:04:13 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,11 @@
+from app.server.app import app
+import uvicorn
+from app.core.config import settings
+
+
+def main():
+    uvicorn.run("main:app", host=settings.APP_HOST, port=settings.APP_PORT, reload=settings.DEBUG, workers=5)
+
+
+if __name__ == "__main__":
+    main()
